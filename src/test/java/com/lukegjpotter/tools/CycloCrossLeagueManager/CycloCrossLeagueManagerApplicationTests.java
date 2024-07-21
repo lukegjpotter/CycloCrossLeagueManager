5bd76bc953c853a5917f3b0b07a9547b67455777
lukegjpotter <lukegjpotter@gmail.com>
2024-07-21 16:57:40 +0100

    Updating Versions.

--- src/test/java/com/lukegjpotter/tools/CycloCrossLeagueManager/CycloCrossLeagueManagerApplicationTests.java
+++ src/test/java/com/lukegjpotter/tools/CycloCrossLeagueManager/CycloCrossLeagueManagerApplicationTests.java
@@ -1,9 +1,11 @@
 package com.lukegjpotter.tools.CycloCrossLeagueManager;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Order(1)
 class CycloCrossLeagueManagerApplicationTests {
 
     @Test
