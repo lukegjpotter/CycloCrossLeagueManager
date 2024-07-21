4f93a8891c8672850e85192496280a5ee9b7468c
lukegjpotter <lukegjpotter@gmail.com>
2024-07-21 21:24:51 +0100

    Correcting GriddingControllerTest

--- src/test/java/com/lukegjpotter/tools/cyclocrossleaguemanager/gridding/controller/GriddingControllerTest.java
+++ src/test/java/com/lukegjpotter/tools/cyclocrossleaguemanager/gridding/controller/GriddingControllerTest.java
@@ -1,6 +1,7 @@
 package com.lukegjpotter.tools.cyclocrossleaguemanager.gridding.controller;
 
 import com.lukegjpotter.tools.cyclocrossleaguemanager.gridding.dto.GriddingRequestRecord;
+import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -8,24 +9,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.emptyString;
+import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class GriddingControllerTest {
 
     @BeforeAll
     static void beforeClass() {
-        baseURI = "http://localhost:8080/lotto";
+        baseURI = "http://localhost:8080/";
     }
 
     @Test
     void gridRace() {
         given()
+                .contentType(ContentType.JSON)
                 .body(new GriddingRequestRecord("docs.google.com/spreadsheet/123"))
                 .when()
                 .post("/gridding")
                 .then()
                 .statusCode(HttpStatus.SC_OK)
-                .body("googleSheet", contains("docs.google.com/spreadsheet/456"));
+                .body("googleSheet", equalTo("docs.google.com/spreadsheet/456"),
+                        "errorMessage", emptyString());
     }
 }
