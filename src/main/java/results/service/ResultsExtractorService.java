f0e2b4250de7d408a224a76f089310e1124c901b
lukegjpotter <lukegjpotter@gmail.com>
2024-07-21 16:58:02 +0100

    Basic ResultsExtractorService Tests

--- src/main/java/results/service/ResultsExtractorService.java
+++ src/main/java/results/service/ResultsExtractorService.java
@@ -1,9 +1,13 @@
 package results.service;
 
 import common.model.TransformedResults;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class ResultsExtractorService {
@@ -12,6 +16,16 @@ public class ResultsExtractorService {
 
     public TransformedResults extract(String raceResultsUrl) {
         // TODO: Start jSoup Extracting RaceResults results webpage.
-        return null;
+        Document document = null;
+        try {
+            document = Jsoup.connect(raceResultsUrl).get();
+        } catch (IOException e) {
+            return null;
+        }
+        TransformedResults transformedResults = new TransformedResults();
+        String round = document.body().getElementsByTag("thbody").get(0).getElementsByTag("tr").get(1).text().trim();
+        transformedResults.setRound(round);
+
+        return transformedResults;
     }
 }
--- src/test/java/results/service/ResultsExtractorServiceTest.java
+++ src/test/java/results/service/ResultsExtractorServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class ResultsExtractorServiceTest {
 
     @Autowired
@@ -15,5 +17,9 @@ class ResultsExtractorServiceTest {
 
     @Test
     void extract() {
+        String expected = "Round 3";
+        String actual = resultsExtractorService.extract("").getRound();
+
+        assertEquals(expected, actual, "Expected: " + expected + ", but got: " + actual);
     }
 }
