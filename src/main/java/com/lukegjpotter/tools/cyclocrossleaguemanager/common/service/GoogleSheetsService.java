d4d95019e68511607591b826144ed498f789424d
lukegjpotter <lukegjpotter@gmail.com>
2024-07-26 00:09:01 +0100

    Refactoring Google Sheets.

---
+++ src/main/java/com/lukegjpotter/tools/cyclocrossleaguemanager/common/service/GoogleSheetsService.java
@@ -0,0 +1,62 @@
+package com.lukegjpotter.tools.cyclocrossleaguemanager.common.service;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.lukegjpotter.tools.cyclocrossleaguemanager.gridding.repository.component.SheetsQuickstart;
+import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class GoogleSheetsService {
+
+    private final Sheets googleSheets;
+
+    public GoogleSheetsService() throws GeneralSecurityException, IOException {
+        // Build a new authorized API client service.
+        final String APPLICATION_NAME = "Google Sheets API Java Quickstart";
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+
+        final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+        final String TOKENS_DIRECTORY_PATH = "./src/main/resources/";
+        final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS_READONLY);
+        final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+        // Load client secrets.
+        InputStream in = SheetsQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+
+        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+
+        this.googleSheets = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+    public Sheets getGoogleSheets() {
+        return googleSheets;
+    }
+}
