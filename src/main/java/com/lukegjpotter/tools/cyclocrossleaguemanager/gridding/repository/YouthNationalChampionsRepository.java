c61d9cef8d619400575fc2e6f66ac33e13c624e4
lukegjpotter <lukegjpotter@gmail.com>
2024-07-31 21:42:23 +0100

    Implementing YouthNationalChampionsRepository

--- src/main/java/com/lukegjpotter/tools/cyclocrossleaguemanager/gridding/repository/YouthNationalChampionsRepository.java
+++ src/main/java/com/lukegjpotter/tools/cyclocrossleaguemanager/gridding/repository/YouthNationalChampionsRepository.java
@@ -2,18 +2,35 @@ package com.lukegjpotter.tools.cyclocrossleaguemanager.gridding.repository;
 
 import com.lukegjpotter.tools.cyclocrossleaguemanager.gridding.model.BookingReportRowRecord;
 import com.lukegjpotter.tools.cyclocrossleaguemanager.gridding.model.RiderGriddingPositionRecord;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvValidationException;
 import org.springframework.stereotype.Repository;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 @Repository
 public class YouthNationalChampionsRepository {
 
-    // ToDo: Webservice call to get the U16 and U14 National Champions.
+    public List<RiderGriddingPositionRecord> findAll(List<BookingReportRowRecord> signupsBookingReportList) {
 
-    public Collection<RiderGriddingPositionRecord> findAll(List<BookingReportRowRecord> all) {
-        return new ArrayList<>();
+        List<RiderGriddingPositionRecord> youthNationalChampions = new ArrayList<>();
+
+        try (CSVReader csvReader = new CSVReaderBuilder(new FileReader("src/main/resources/YouthNationalChampions.csv")).withSkipLines(1).build()) {
+            String[] lineInArray;
+
+            while ((lineInArray = csvReader.readNext()) != null) {
+                RiderGriddingPositionRecord youthNationalChampion = new RiderGriddingPositionRecord(lineInArray[0].trim(), 1, lineInArray[1].trim(), lineInArray[2].trim());
+                if (signupsBookingReportList.contains(new BookingReportRowRecord(youthNationalChampion.raceCategory(), youthNationalChampion.fullName(), youthNationalChampion.clubName())))
+                    youthNationalChampions.add(youthNationalChampion);
+            }
+        } catch (IOException | CsvValidationException e) {
+            return youthNationalChampions;
+        }
+
+        return youthNationalChampions;
     }
 }
--- src/test/java/com/lukegjpotter/tools/cyclocrossleaguemanager/gridding/repository/YouthNationalChampionsRepositoryTest.java
+++ src/test/java/com/lukegjpotter/tools/cyclocrossleaguemanager/gridding/repository/YouthNationalChampionsRepositoryTest.java
@@ -1,14 +1,59 @@
 package com.lukegjpotter.tools.cyclocrossleaguemanager.gridding.repository;
 
+import com.lukegjpotter.tools.cyclocrossleaguemanager.gridding.model.BookingReportRowRecord;
+import com.lukegjpotter.tools.cyclocrossleaguemanager.gridding.model.RiderGriddingPositionRecord;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class YouthNationalChampionsRepositoryTest {
 
-    //@Test
-    void findAll() {
-        fail("Not yet Implemented");
+    @Autowired
+    YouthNationalChampionsRepository youthNationalChampionsRepository;
+
+    @Test
+    void findAll_AllAreRacing() {
+        List<RiderGriddingPositionRecord> expected = List.of(
+                new RiderGriddingPositionRecord("Boy's U16", 1, "Curtis McKee", "Spellman-Dublin Port"),
+                new RiderGriddingPositionRecord("Boy's U14", 1, "James Cunningham", "Orwell Wheelers Cycling Club"),
+                new RiderGriddingPositionRecord("Girl's U16", 1, "Emer Heverin", "Kinning Cycles Cycling Club"),
+                new RiderGriddingPositionRecord("Girl's U14", 1, "Katie Turner", "Orwell Wheelers Cycling Club"));
+        List<RiderGriddingPositionRecord> actual = youthNationalChampionsRepository.findAll(new ArrayList<>(List.of(
+                new BookingReportRowRecord("Boy's U16", "Curtis McKee", "Spellman-Dublin Port"),
+                new BookingReportRowRecord("Boy's U14", "James Cunningham", "Orwell Wheelers Cycling Club"),
+                new BookingReportRowRecord("Girl's U16", "Emer Heverin", "Kinning Cycles Cycling Club"),
+                new BookingReportRowRecord("Girl's U14", "Katie Turner", "Orwell Wheelers Cycling Club"))));
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findAll_SomeAreRacing() {
+        List<RiderGriddingPositionRecord> expected = List.of(
+                new RiderGriddingPositionRecord("Boy's U16", 1, "Curtis McKee", "Spellman-Dublin Port"),
+                new RiderGriddingPositionRecord("Girl's U14", 1, "Katie Turner", "Orwell Wheelers Cycling Club"));
+        List<RiderGriddingPositionRecord> actual = youthNationalChampionsRepository.findAll(new ArrayList<>(List.of(
+                new BookingReportRowRecord("A-Race", "John McIntosh", "Round Wheelers"),
+                new BookingReportRowRecord("Boy's U16", "Curtis McKee", "Spellman-Dublin Port"),
+                new BookingReportRowRecord("Girl's U14", "Katie Turner", "Orwell Wheelers Cycling Club"),
+                new BookingReportRowRecord("B-Race", "Turtle McIntosh", "Shell Wheelers"))));
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findAll_NoneAreRacing() {
+        List<RiderGriddingPositionRecord> expected = new ArrayList<>();
+        List<RiderGriddingPositionRecord> actual = youthNationalChampionsRepository.findAll(new ArrayList<>(List.of(
+                new BookingReportRowRecord("A-Race", "John McIntosh", "Round Wheelers"),
+                new BookingReportRowRecord("B-Race", "Turtle McIntosh", "Shell Wheelers"))));
+
+        assertEquals(expected, actual);
     }
 }
