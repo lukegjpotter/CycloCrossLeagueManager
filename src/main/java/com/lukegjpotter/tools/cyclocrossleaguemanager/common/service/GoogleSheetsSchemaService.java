fd912d5501d2ea87c2bde32d9c721387fecfdc79
lukegjpotter <lukegjpotter@gmail.com>
2025-11-17 17:41:10 +0000

    Continuing to centralise the Race Category Naming.

--- src/main/java/com/lukegjpotter/tools/cyclocrossleaguemanager/common/service/GoogleSheetsSchemaService.java
+++ src/main/java/com/lukegjpotter/tools/cyclocrossleaguemanager/common/service/GoogleSheetsSchemaService.java
@@ -27,12 +27,12 @@ public class GoogleSheetsSchemaService {
                 new GriddingRaceType(raceCategoryNameService.aRace(), "B4", 24),
                 new GriddingRaceType(raceCategoryNameService.women(), "E4", 24),
                 new GriddingRaceType(raceCategoryNameService.bRace(), "H4", 24),
-                new GriddingRaceType(raceCategoryNameService.u16Male(), "B31", 8),
-                new GriddingRaceType(raceCategoryNameService.u16Female(), "E31", 8),
-                new GriddingRaceType(raceCategoryNameService.u14Male(), "B42", 8),
-                new GriddingRaceType(raceCategoryNameService.u14Female(), "E42", 8),
-                new GriddingRaceType(raceCategoryNameService.u12Male(), "H31", 8),
-                new GriddingRaceType(raceCategoryNameService.u12Female(), "H42", 8));
+                new GriddingRaceType(raceCategoryNameService.under16sMale(), "B31", 8),
+                new GriddingRaceType(raceCategoryNameService.under16sFemale(), "E31", 8),
+                new GriddingRaceType(raceCategoryNameService.under14sMale(), "B42", 8),
+                new GriddingRaceType(raceCategoryNameService.under14sFemale(), "E42", 8),
+                new GriddingRaceType(raceCategoryNameService.under12sMale(), "H31", 8),
+                new GriddingRaceType(raceCategoryNameService.under12sFemale(), "H42", 8));
     }
 
     public List<LeagueStandingsRaceType> leagueStandingsSchema() {
@@ -40,12 +40,12 @@ public class GoogleSheetsSchemaService {
                 new LeagueStandingsRaceType(raceCategoryNameService.aRace()),
                 new LeagueStandingsRaceType(raceCategoryNameService.women()),
                 new LeagueStandingsRaceType(raceCategoryNameService.bRace()),
-                new LeagueStandingsRaceType("U16 Male"),
-                new LeagueStandingsRaceType("U16 Female"),
-                new LeagueStandingsRaceType("U14 Male"),
-                new LeagueStandingsRaceType("U14 Female"),
-                new LeagueStandingsRaceType("U12 Male"),
-                new LeagueStandingsRaceType("U12 Female"));
+                new LeagueStandingsRaceType(raceCategoryNameService.u16Male()),
+                new LeagueStandingsRaceType(raceCategoryNameService.u16Female()),
+                new LeagueStandingsRaceType(raceCategoryNameService.u14Male()),
+                new LeagueStandingsRaceType(raceCategoryNameService.u14Female()),
+                new LeagueStandingsRaceType(raceCategoryNameService.u12Male()),
+                new LeagueStandingsRaceType(raceCategoryNameService.u12Female()));
     }
 
     public LeagueStandingsHeader leagueStandingsHeaders() {
--- src/main/java/com/lukegjpotter/tools/cyclocrossleaguemanager/common/service/RaceCategoryNameService.java
+++ src/main/java/com/lukegjpotter/tools/cyclocrossleaguemanager/common/service/RaceCategoryNameService.java
@@ -17,27 +17,51 @@ public class RaceCategoryNameService {
         return "Women";
     }
 
-    public String u16Male() {
+    public String under16sMale() {
         return "Under 16s Male";
     }
 
-    public String u16Female() {
+    public String under16sFemale() {
         return "Under 16s Female";
     }
 
-    public String u14Male() {
+    public String under14sMale() {
         return "Under 14s Male";
     }
 
-    public String u14Female() {
+    public String under14sFemale() {
         return "Under 14s Female";
     }
 
-    public String u12Male() {
+    public String under12sMale() {
         return "Under 12s Male";
     }
 
-    public String u12Female() {
+    public String under12sFemale() {
         return "Under 12s Female";
+    }
+
+    public String u16Male() {
+        return "U16 Male";
+    }
+
+    public String u16Female() {
+        return "U16 Female";
+    }
+
+    public String u14Male() {
+        return "U14 Male";
+    }
+
+    public String u14Female() {
+        return "U14 Female";
+    }
+
+    public String u12Male() {
+        return "U12 Male";
+    }
+
+    public String u12Female() {
+        return "U12 Female";
     }
 }
