bfbd537ec9586ca16e63ecd2a684d10d895c57a3
lukegjpotter <lukegjpotter@gmail.com>
2025-11-26 13:59:33 +0000

    Mocking out the Google Sheets Calls. The test suite is causing a Rate Limit Exceeded Exception, so it's best to have the stable tests Mocked.

--- src/test/java/com/lukegjpotter/tools/cyclocrossleaguemanager/gridding/repository/BookingReportRepositoryTest.java
+++ src/test/java/com/lukegjpotter/tools/cyclocrossleaguemanager/gridding/repository/BookingReportRepositoryTest.java
@@ -1,21 +1,30 @@
 package com.lukegjpotter.tools.cyclocrossleaguemanager.gridding.repository;
 
+import com.lukegjpotter.tools.cyclocrossleaguemanager.common.service.GoogleSheetsService;
 import com.lukegjpotter.tools.cyclocrossleaguemanager.gridding.model.BookingReportRowRecord;
+import com.lukegjpotter.tools.cyclocrossleaguemanager.testutils.GriddingTestMocks;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.io.IOException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class BookingReportRepositoryTest {
 
     @Autowired
     private BookingReportRepository bookingReportRepository;
+    @Autowired
+    private GriddingTestMocks griddingTestMocks;
+
+    @MockitoBean
+    private GoogleSheetsService googleSheetsService;
 
     @BeforeEach
     void setUp() {
@@ -23,6 +32,9 @@ class BookingReportRepositoryTest {
 
     @Test
     void getDataFromSignUpsGoogleSheet() throws IOException {
+        when(googleSheetsService.getSpreadsheetHeaders("1yn-ws9qNVrm0H268gLsN0VRaVN5jSqjuKd2VMPCU_uc", "Report")).thenReturn(griddingTestMocks.getBookingReportHeaders());
+        when(googleSheetsService.readSpreadsheetValuesInRange("1yn-ws9qNVrm0H268gLsN0VRaVN5jSqjuKd2VMPCU_uc", "Report!F2:AB")).thenReturn(griddingTestMocks.getBookingReport2Values());
+
         List<BookingReportRowRecord> actual = bookingReportRepository.getDataFromSignUpsGoogleSheet("1yn-ws9qNVrm0H268gLsN0VRaVN5jSqjuKd2VMPCU_uc", false).subList(0, 6);
 
         List<BookingReportRowRecord> expected = List.of(
