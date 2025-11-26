fac6031fba6249dda3e4a5e8091124cf595ff483
lukegjpotter <lukegjpotter@gmail.com>
2025-11-26 15:28:44 +0000

    Reformatting.

--- src/test/java/com/lukegjpotter/tools/cyclocrossleaguemanager/testutils/GriddingTestMocks.java
+++ src/test/java/com/lukegjpotter/tools/cyclocrossleaguemanager/testutils/GriddingTestMocks.java
@@ -54,53 +54,49 @@ public class GriddingTestMocks {
 
         logger.trace("Getting Mocked Booking Report 2024 Values.");
 
-        return new ValueRange().setValues(
-                List.of(
-                        List.of("A race", "Andy", "Aracer", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Dublin 1", "00353-87-1111111", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40069", "Emerald Wheelers"),
-                        List.of("B race", "Billy", "Bracer", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Dublin 2", "00353-87-1111112", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40070", "Shamrock Cycling Club"),
-                        List.of("B race Junior", "Johnny", "Junior", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Dublin 3", "00353-87-1111113", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40071", "Celtic Pedalers"),
-                        List.of("Women", "Wanda", "Wracer", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Dublin 6W", "00353-87-1111114", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40072", "Gael Riders"),
-                        List.of("Women", "Martha", "Matherson", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Meath", "00353-87-1111115", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40073", "Dublin Spinners"),
-                        List.of("Under 12s", "Aoife", "O'Sullivan", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Meath", "00353-87-1111116", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40074", "Claddagh Cyclists"),
-                        List.of("B race", "Liam", "Murphy", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Meath", "00353-87-1111117", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40075", "Limerick Rollers"),
-                        List.of("Under 12s", "Niamh", "Gallagher", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Kildare", "00353-87-1111118", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40076", "Galway Roadsters"),
-                        List.of("A race", "Conor", "Byrne", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Kildare", "00353-87-1111119", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40077", "Belfast Bikers"),
-                        List.of("Women Junior", "Siobhán", "O'Reilly", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Kildare", "00353-87-1111120", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40078", "Kerry Cruisers"),
-                        List.of("A race", "Rory", "McDonagh", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Kildare", "00353-87-1111121", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40079", "Emerald Wheelers"),
-                        List.of("Women", "Caoimhe", "Brennan", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Cavan", "00353-87-1111122", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40080", "Shamrock Cycling Club"),
-                        List.of("Under 12s", "Eoin", "Fitzpatrick", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Meath", "00353-87-1111123", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40081", "Celtic Pedalers"),
-                        List.of("Under 14s", "Orlaith", "Kelly", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Louth", "00353-87-1111124", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40082", "Gael Riders"),
-                        List.of("B race", "Cian", "Doyle", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Monaghan", "00353-87-1111125", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40083", "Dublin Spinners"),
-                        List.of("Under 10s", "Fiona", "McCarthy", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Louth", "00353-87-1111126", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40084", "Claddagh Cyclists"),
-                        List.of("A race", "Seán", "Kavanagh", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Louth", "00353-87-1111127", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40085", "Limerick Rollers"),
-                        List.of("Women", "Aisling", "Keane", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Louth", "00353-87-1111128", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40086", "Galway Roadsters"),
-                        List.of("Under 14s", "Patrick", "Duffy", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Meath", "00353-87-1111129", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40087", "Belfast Bikers"),
-                        List.of("Under 16s", "Maeve", "Nolan", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Wexford", "00353-87-1111130", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40088", "Kerry Cruisers"),
-                        List.of("Under 12s", "Declan", "O'Connor", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Kildare", "00353-87-1111131", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40089", "Emerald Wheelers"),
-                        List.of("Under 14s", "Gráinne", "Quinn", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Kildare", "00353-87-1111132", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40090", "Shamrock Cycling Club"),
-                        List.of("B race Junior", "Darragh", "O'Malley", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Dublin", "00353-87-1111133", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40091", "Celtic Pedalers"),
-                        List.of("Under 16s", "Bríd", "Flynn", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Dublin", "00353-87-1111134", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40092", "Gael Riders"),
-                        List.of("Under 14s", "Eoghan", "Ryan", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Dublin 3", "00353-87-1111135", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40093", "Dublin Spinners")
-                )
-        );
+        return new ValueRange().setValues(List.of(
+                List.of("A race", "Andy", "Aracer", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Dublin 1", "00353-87-1111111", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40069", "Emerald Wheelers"),
+                List.of("B race", "Billy", "Bracer", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Dublin 2", "00353-87-1111112", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40070", "Shamrock Cycling Club"),
+                List.of("B race Junior", "Johnny", "Junior", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Dublin 3", "00353-87-1111113", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40071", "Celtic Pedalers"),
+                List.of("Women", "Wanda", "Wracer", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Dublin 6W", "00353-87-1111114", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40072", "Gael Riders"),
+                List.of("Women", "Martha", "Matherson", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Meath", "00353-87-1111115", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40073", "Dublin Spinners"),
+                List.of("Under 12s", "Aoife", "O'Sullivan", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Meath", "00353-87-1111116", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40074", "Claddagh Cyclists"),
+                List.of("B race", "Liam", "Murphy", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Meath", "00353-87-1111117", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40075", "Limerick Rollers"),
+                List.of("Under 12s", "Niamh", "Gallagher", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Kildare", "00353-87-1111118", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40076", "Galway Roadsters"),
+                List.of("A race", "Conor", "Byrne", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Kildare", "00353-87-1111119", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40077", "Belfast Bikers"),
+                List.of("Women Junior", "Siobhán", "O'Reilly", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Kildare", "00353-87-1111120", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40078", "Kerry Cruisers"),
+                List.of("A race", "Rory", "McDonagh", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Kildare", "00353-87-1111121", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40079", "Emerald Wheelers"),
+                List.of("Women", "Caoimhe", "Brennan", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Cavan", "00353-87-1111122", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40080", "Shamrock Cycling Club"),
+                List.of("Under 12s", "Eoin", "Fitzpatrick", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Meath", "00353-87-1111123", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40081", "Celtic Pedalers"),
+                List.of("Under 14s", "Orlaith", "Kelly", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Louth", "00353-87-1111124", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40082", "Gael Riders"),
+                List.of("B race", "Cian", "Doyle", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Monaghan", "00353-87-1111125", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40083", "Dublin Spinners"),
+                List.of("Under 10s", "Fiona", "McCarthy", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Louth", "00353-87-1111126", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40084", "Claddagh Cyclists"),
+                List.of("A race", "Seán", "Kavanagh", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Louth", "00353-87-1111127", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40085", "Limerick Rollers"),
+                List.of("Women", "Aisling", "Keane", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Louth", "00353-87-1111128", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40086", "Galway Roadsters"),
+                List.of("Under 14s", "Patrick", "Duffy", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Meath", "00353-87-1111129", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40087", "Belfast Bikers"),
+                List.of("Under 16s", "Maeve", "Nolan", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Wexford", "00353-87-1111130", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40088", "Kerry Cruisers"),
+                List.of("Under 12s", "Declan", "O'Connor", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Kildare", "00353-87-1111131", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40089", "Emerald Wheelers"),
+                List.of("Under 14s", "Gráinne", "Quinn", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Kildare", "00353-87-1111132", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40090", "Shamrock Cycling Club"),
+                List.of("B race Junior", "Darragh", "O'Malley", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Dublin", "00353-87-1111133", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40091", "Celtic Pedalers"),
+                List.of("Under 16s", "Bríd", "Flynn", "email@email.com", "email@email.com", "Female", "04.10.1974", "50", "Dublin", "00353-87-1111134", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40092", "Gael Riders"),
+                List.of("Under 14s", "Eoghan", "Ryan", "email@email.com", "email@email.com", "Male", "04.10.1974", "50", "Dublin 3", "00353-87-1111135", "", "", "", "", "", "Dee Znuts", "872222222", "", "", "", "yes", "24A40093", "Dublin Spinners")
+        ));
     }
 
     public ValueRange getBookingReport2025Values() {
 
         logger.trace("Getting Mocked Booking Report 2025 Values.");
 
-        return new ValueRange().setValues(
-                List.of(
-                        List.of("Junior Women / U23 women", "Sarah", "McPedaler", "email@email.com", "email@email.com", "Female", "01.01.2008", "17", "", "", "", "", "", "", "", "yes", "yes", "yes", "", "CT Racing"),
-                        List.of("Junior A race / U23", "Sean", "O'Chainring", "email@email.com", "email@email.com", "Male", "02.01.2008", "17", "", "", "", "", "", "", "", "yes", "no", "yes", "", "West Dublin Racing"),
-                        List.of("Junior B race / U23", "Luke", "Bottom-Bracket", "email@email.com", "email@email.com", "Male", "03.01.2008", "17", "", "", "", "", "", "", "", "yes", "yes", "no", "", "West Dublin Racing"),
-                        List.of("A race M60", "Peter", "Peterson", "email@email.com", "email@email.com", "Male", "04.01.2008", "17", "", "", "", "", "", "", "", "yes", "no", "no", "", "Wicklow Pedalers"),
-                        List.of("A race (non licence)", "Billy", "Brown", "email@email.com", "email@email.com", "Male", "05.01.2008", "17", "", "", "", "", "", "", "", "yes", "no", "no", ""),
-                        List.of("Under 16s", "James", "Jameson", "email@email.com", "email@email.com", "Male", "06.01.2008", "17", "", "", "", "", "", "", "", "yes", "no", "no", "", "South-Dublin Wheelers"),
-                        List.of("Under 14s", "Jake", "Jakeson", "email@email.com", "email@email.com", "Male", "07.01.2008", "17", "", "", "", "", "", "", "", "yes", "no", "no", "", "South-Dublin Wheelers"),
-                        List.of("Youth U12 (2014-2015) (non-licence)", "Nathan", "Fillion", "email@email.com", "email@email.com", "Male", "08.01.2008", "17", "", "", "", "", "", "", "", "yes", "no", "yes", "", "Castle Wheelers")
-                )
-        );
+        return new ValueRange().setValues(List.of(
+                List.of("Junior Women / U23 women", "Sarah", "McPedaler", "email@email.com", "email@email.com", "Female", "01.01.2008", "17", "", "", "", "", "", "", "", "yes", "yes", "yes", "", "CT Racing"),
+                List.of("Junior A race / U23", "Sean", "O'Chainring", "email@email.com", "email@email.com", "Male", "02.01.2008", "17", "", "", "", "", "", "", "", "yes", "no", "yes", "", "West Dublin Racing"),
+                List.of("Junior B race / U23", "Luke", "Bottom-Bracket", "email@email.com", "email@email.com", "Male", "03.01.2008", "17", "", "", "", "", "", "", "", "yes", "yes", "no", "", "West Dublin Racing"),
+                List.of("A race M60", "Peter", "Peterson", "email@email.com", "email@email.com", "Male", "04.01.2008", "17", "", "", "", "", "", "", "", "yes", "no", "no", "", "Wicklow Pedalers"),
+                List.of("A race (non licence)", "Billy", "Brown", "email@email.com", "email@email.com", "Male", "05.01.2008", "17", "", "", "", "", "", "", "", "yes", "no", "no", ""),
+                List.of("Under 16s", "James", "Jameson", "email@email.com", "email@email.com", "Male", "06.01.2008", "17", "", "", "", "", "", "", "", "yes", "no", "no", "", "South-Dublin Wheelers"),
+                List.of("Under 14s", "Jake", "Jakeson", "email@email.com", "email@email.com", "Male", "07.01.2008", "17", "", "", "", "", "", "", "", "yes", "no", "no", "", "South-Dublin Wheelers"),
+                List.of("Youth U12 (2014-2015) (non-licence)", "Nathan", "Fillion", "email@email.com", "email@email.com", "Male", "08.01.2008", "17", "", "", "", "", "", "", "", "yes", "no", "yes", "", "Castle Wheelers")
+        ));
     }
 
     public List<RiderUciPointRecord> getUciPointsFor20252026() {
@@ -135,5 +131,18 @@ public class GriddingTestMocks {
                 new RiderUciPointRecord("WE", "Rachel Newell", 10),
                 new RiderUciPointRecord("WE", "Fiona Lally", 5)
         );
+    }
+
+    public List<String> getLeagueStandingsHeaders() {
+        return List.of("Pos.", "Name", "Club", "Cat", "R1", "R2", "R3", "R4", "Total", "Rd 1 Total", "Rd 2 Total", "Rd 3 Total", "Rd 4 Total");
+    }
+
+    public ValueRange getLeagueStandings2024() {
+        return new ValueRange().setValues(List.of(
+                List.of("", "", ""),
+                List.of("", "", ""),
+                List.of("", "", ""),
+                List.of("", "", "")
+        ));
     }
 }
